173ad3d4cddf488de70bd757888cc27ad3a9f2d2
Eka Oktavianus <47300684+eoktavianus@users.noreply.github.com>
2023-11-30 21:29:55 +0700

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+employ_merge = pd.read_pickle('data/employ_clean.pkl')
+coord = pd.read_csv('data/coordinate.csv')
+
+# --- Row1 - Title & Deskripsi ---
+# st.title("Employee Demography Dashboard")
+st.write("# Employee Demography Dashboard")
+st.write("""Explore workforce dynamics at a glance with this Employee Demography Dashboard. 
+            Track hiring trends over time, visualize employee distribution across provinces, 
+            and delve into generational and gender insights within departments—all in one comprehensive view.""")
+
+#  --- Row 2 ---
+col1, col2 = st.columns(2)
+# data line plot
+df_join = pd.crosstab(index=employ_merge['join_year'],
+                      columns='join_count',
+                      colnames=[None])
+df_join = df_join.reset_index()
+
+# plot: line plot
+plot_join = px.line(df_join, x='join_year', y = 'join_count', markers=True,
+                    labels = {
+                        'join_year' : 'Year',
+                        'join_count' : 'Employee Joining'
+                    })
+col1.write("### Joining Frequency Over Time")
+col1.plotly_chart(plot_join, use_container_width=True)
+
+## --- MAP PLOT ---
+# data: map
+prov_gender = pd.crosstab(index=employ_merge['province'],
+                          columns=employ_merge['gender'], colnames=[None])  
+prov_gender['Total'] = prov_gender['Female'] + prov_gender['Male']
+df_map = prov_gender.merge(coord, on='province')
+
+# plot:map  #  mapbox_style='open-street-map', zoom = 3,
+plot_map = px.scatter_mapbox(data_frame=df_map, lat='latitude', lon='longitude',                            
+                            mapbox_style='carto-positron', zoom = 3,
+                             size='Total',
+                             hover_name='province',
+                             hover_data={
+                                 'Male': True,
+                                 'Female': True,
+                                 'latitude': True,
+                                 'longitude': True
+                             })
+col2.write("### Employee Count Across Indonesia")
+col2.plotly_chart(plot_map, use_container_width=True)
+
+#  --- Row 3 ---
+st.divider()
+col3, col4 = st.columns(2)
+
+## --- INPUT SELECT ---
+input_select = col3.selectbox(
+    label='Select Department',
+    options=employ_merge['department_name'].unique().sort_values()
+)
+
+## --- IN{UT SLIDER ---
+input_slider = col4.slider(
+    label='Select Age Rabge',
+    min_value=employ_merge['age'].min(),
+    max_value=employ_merge['age'].max(),
+    value=[20,50]
+)
+
+min_slider = input_slider[0]
+max_slider = input_slider[1]
+
+#  --- Row 4 ---
+col5, col6 = st.columns(2)
+
+## --- BAR PLOT ---
+# data: bar plot
+employ_cs = employ_merge[employ_merge['department_name'] == input_select]
+df_gen = pd.crosstab(index=employ_cs['generation'], columns='num_people', colnames=[None])
+df_gen = df_gen.reset_index()
+
+#  plot:barplot
+plot_gen = px.bar(df_gen, x='generation', y='num_people',
+                  labels={'generation' : 'Generation',
+                          'num_people' : 'Employee Count'})
+col5.write(f'### ')
+
+#  --- Row 5 ---
